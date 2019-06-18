26f29f139ff7f12626c5006e9213b44d717e283f
Dayana Román <daromanca@unal.edu.co>
2019-06-18 01:20:20 -0500

    Create Pregunta12_ocupaciones_estadosunidos.sql

---
+++ Querys/Pregunta12_ocupaciones_estadosunidos.sql
@@ -0,0 +1,14 @@
+-- PREGUNTA 12: ¿Cuál ha sido el comportamiento histórico por ocupación en Estados Unidos ?
+
+SELECT u.pais, t.año, d.[Database Administrator], d.[Desktop Developer], d.[Embedded Developer], d.[System Developer], d.[Web Developer]
+FROM
+(
+	SELECT d.FK_tiempo, d.FK_ubicacion, SUM(ocupacion_Database) [Database Administrator], SUM(ocupacion_Desktop) [Desktop Developer], SUM(ocupacion_Embedded) [Embedded Developer], SUM(ocupacion_Staff) [System Developer],
+			SUM(ocupacion_Web) [Web Developer]
+	FROM dw_stackoverflow d
+	GROUP BY d.FK_tiempo, d.FK_ubicacion
+) d
+INNER JOIN tiempo t ON t.id_tiempo = d.FK_tiempo
+INNER JOIN ubicacion u ON u.id_ubicacion = d.FK_ubicacion
+WHERE pais = 'United States' AND t.año >= 2014
+ORDER BY u.pais, t.año 

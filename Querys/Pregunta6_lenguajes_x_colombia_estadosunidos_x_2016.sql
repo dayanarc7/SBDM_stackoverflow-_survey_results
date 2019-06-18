2fa9ab3553627977a76d835ab9b177ec5fd7d6ff
Dayana Román <daromanca@unal.edu.co>
2019-06-17 23:24:53 -0500

    Update Pregunta6_lenguajes_x_colombia_estadosunidos_x_2016.sql

--- Querys/Pregunta6_lenguajes_x_colombia_estadosunidos_x_2016.sql
+++ Querys/Pregunta6_lenguajes_x_colombia_estadosunidos_x_2016.sql
@@ -1,10 +1,12 @@
 -- PREGUNTA 6: ¿Cuál ha sido el comportamiento entre Colombia y Estados Unidos según cada lenguaje para el año 2016 ?
 
-SELECT u.pais, t.año, d.Java, d.Python, d.C#, d.[C++], d.Javascript, d.Php, d.Ruby, d.Sql 
+
+SELECT u.pais, t.año, d.Java * 100.0 /d.total [Java] , d.Python *100.1 /d.total [Python], d.C# *100.1/ d.total [C#], d.[C++] *100.1 /d.total [C++], d.Javascript*100.0/ d.total [Javascript], d.Php *100.0 /d.total [PHP], d.Ruby *100.0/d.total [Ruby], d.Sql *100.0 /d.total [Sql]
 FROM
 (
 	SELECT d.FK_tiempo, d.FK_ubicacion, SUM(lenguaje_java) [Java], SUM(lenguaje_python) [Python], SUM(lenguaje_c#) [C#], SUM([lenguaje_c++]) [C++],
-			SUM(lenguaje_javascript) [Javascript], SUM(lenguaje_php) [Php], SUM(lenguaje_ruby) [Ruby], SUM(lenguaje_sql) [Sql]
+			SUM(lenguaje_javascript) [Javascript], SUM(lenguaje_php) [Php], SUM(lenguaje_ruby) [Ruby], SUM(lenguaje_sql) [Sql],
+			 SUM(lenguaje_java) + SUM(lenguaje_python) + SUM(lenguaje_c#) + SUM([lenguaje_c++]) + SUM(lenguaje_javascript) + SUM(lenguaje_php) + SUM(lenguaje_ruby) + SUM(lenguaje_sql) total
 	FROM dw_stackoverflow d
 	GROUP BY d.FK_tiempo, d.FK_ubicacion
 ) d
